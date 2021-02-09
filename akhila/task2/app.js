8edd0cd512fcb83b67a9a79726422df6421ea4b8
Mahammad Basha <mahammad.basha@blancco.com>
2021-02-09 12:00:05 +0530

    task2 .... promise excercise

--- akhila/task2/app.js
+++ akhila/task2/app.js
@@ -1,12 +1,58 @@
 function major() {
-    let x = display();
-
-    document.getElementById("i").innerHTML = x;
+    
+    // expects callback... takes time to execute...or fetch data.
+    // getResourceFromInternet(function(rn){
+    //     console.log("rn --> ", rn)
+    // });
+    
+    getRnPromise() // this function is returning a promise.....
+        .then(
+            function(data){
+                console.log("random number recieved is --> ", data)
+            }
+        )
+        .catch(
+            function(){
+                console.log("this is reject function")
+            }
+        )
 }
 
-setTimeout(display, 5000);
+//asynchronous function --> time
+function getResourceFromInternet(cb) {
+    let y
+    
+    setTimeout(() => {
+        y = Math.random();
+        console.log("random number y in getResourceFromInternet: ", y);
+        console.log("cb is --> ", cb);
+        cb(y);
+    }, 3000);
 
-function display() {
-    let y = Math.random();
-    return y;
+    // let apiUrl = "https://something.com/users"
+    // return after 5 seconds
+    return y; // this will be returned immediately...
 }
+
+
+function getRnPromise(){
+    return new Promise(
+        (resolve, reject) => {
+            // call an api.... server gave an error
+            setTimeout(()=>{
+                let y = Math.random();
+                resolve();
+            }, 2000)
+        }
+    );
+}
+
+
+// callback -->
+    // - it is a function
+    // - when is it executed....?
+    //     - after some asynchronous action is completed.
+// Promise
+// async/await
+
+// 2 hours on this....
